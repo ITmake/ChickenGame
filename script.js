646322f9cc116e3f77cc055c475cd73f02c91b8c
ITmake <max.syrjalainen@edu.prakticum.fi>
2025-11-05 13:16:31 +0200

    GetelementbyID INIT Kod

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ let state = {
     active: 0,
     finished: false
   };
+  
+  document.addEventListener('DOMContentLoaded', () => {
+  const targetInput = document.getElementById('target');
+  const highscoreList = document.getElementById('highscore-list');
+
+  function init() {
+    document.getElementById('roll-0').addEventListener('click', () => kasta(0));
+    document.getElementById('roll-1').addEventListener('click', () => kasta(1));
+    document.getElementById('hold-0').addEventListener('click', () => stanna(0));
+    document.getElementById('hold-1').addEventListener('click', () => stanna(1));
+    document.getElementById('new-game').addEventListener('click', resetGame);
+
+    resetGame();
+    loadHighscores();
+  }
+
+  init();
+});
